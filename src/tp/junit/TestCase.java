877e264a5e296a8c906a9b158b9f83a97188155f
Julian <Julian@Luft>
2013-10-26 23:57:34 -0300

    Se lanza excepcion en TestCase si el usuario no lo redefine

---
+++ src/tp/junit/TestCase.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp.junit;
+
+/**
+ *
+ * @author Julian
+ */
+public class TestCase implements Test{
+    
+    @Override
+    public void runTest() {
+        throw new Error();
+    }
+}
