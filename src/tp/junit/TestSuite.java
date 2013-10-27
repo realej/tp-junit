ecc16c10025415ed069c9a4fab635a9c632125df
Julian <Julian@Luft>
2013-10-27 00:07:20 -0300

    Agrego Compuesto - TestSuite

---
+++ src/tp/junit/TestSuite.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp.junit;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Julian
+ */
+public class TestSuite implements Test {
+    private ArrayList<Test> tests = new ArrayList<Test>();
+    
+    @Override
+    public void runTest(){};
+    
+    public void addTest(Test t){};
+    
+    public void run(){};
+    
+}
