fbd5d0c3970359d4e2eb5559515e24441a0f01c1
Julian <Julian@Luft>
2013-10-26 23:51:19 -0300

    Interfaz test para el composite

---
+++ src/tp/junit/Test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp.junit;
+
+public interface Test {
+    
+    /*
+     * Metodo para correr el Test
+     */
+    public void runTest();
+    
+}
