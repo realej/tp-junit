39d96d6822909b1cffe5ad6864a1581665b01a49
Sebastian <>
2013-10-31 02:53:58 -0300

    Ventana ahora muestra los test fallidos.

--- src/tp/junit/Ventana.java
+++ src/tp/junit/Ventana.java
@@ -8,22 +8,34 @@ import java.awt.GridLayout;
 
 public class Ventana {
 
+    JFrame ventana;
+
     public Ventana(int cantidad, int satisfactorios, int fallidos) {
-        JFrame ventana = new JFrame("Resultados");
+        ventana = new JFrame("Resultados");
         JTextField texto1 = new JTextField("Cantidad total de Test: " + Integer.toString(cantidad));
         JTextField texto2 = new JTextField("Cantidad de tests satisfactorios: " + Integer.toString(satisfactorios));
         texto2.setBackground(Color.GREEN);
         JTextField texto3 = new JTextField("Cantidad de tests fallidos: " + Integer.toString(fallidos));
         texto3.setBackground(Color.red);
-        Dimension d = new Dimension();
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setResizable(false);
-        ventana.setLocation((int) ((d.getWidth() / 2) + 290), 50);
+        Dimension d = new Dimension();
+        ventana.setLocation((int) ((d.getWidth() / 2) + 250), 250);
         ventana.setSize(350, 350);
-        ventana.setLayout(new GridLayout(3, 0));
+        ventana.setLayout(new GridLayout(3 + fallidos, 0));
         ventana.add(texto1);
         ventana.add(texto2);
         ventana.add(texto3);
-        ventana.setVisible(true);      
+    }
+
+    public void agregarMetodosErroneos(String nombre) {
+        JTextField error = new JTextField(nombre);
+        error.setBackground(Color.red);
+        ventana.add(error);
+    }
+
+    public void mostrar() {
+        ventana.setVisible(true);
+
     }
 }
