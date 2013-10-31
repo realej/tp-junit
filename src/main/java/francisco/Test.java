f9814dc5034944573882c8f6f58e0bfff6c1a124
francisco <fran_disa@hotmail.com>
2013-10-31 14:40:32 -0300

    Se agregaron los 2 tipos de asserts soportados a la clase Test

--- src/main/java/francisco/Test.java
+++ src/main/java/francisco/Test.java
@@ -3,5 +3,25 @@ package francisco;
 public abstract class Test {
 	
 	public abstract void run(); 
+	
+	protected boolean assertTrue(boolean expresion) {
+		
+		if (expresion == true) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	protected boolean assertFalse(boolean expresion) {
+		
+		if (expresion == false) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 
 }
