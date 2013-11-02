03d07d2decd88955d15f1d4070a65c529b47cc20
Sebastian <>
2013-11-01 23:27:32 -0300

    Agrego resta calculadora

---
+++ src/test/java/aplicacion/testMultiplicacionCalculadora.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion;
+
+import tp.junit.*;
+import static tp.junit.Assert.assertTrue;
+
+/**
+ *
+ * @author Administrador
+ */
+public class testMultiplicacionCalculadora extends TestCase {
+
+    public void metodoPruebaConSetUp() {
+        Calculadora calc = (Calculadora) getSetUp().ObtenerAtributo(0);
+        assertTrue(calc.resta(1, 1) == 0);
+    }
+
+    @Override
+    public void runTest() {
+        metodoPruebaConSetUp();
+    }
+}
