41b5a2d46383b1b56076aa390e1ba8ba0f6baf97
Sebastian <>
2013-11-01 23:28:04 -0300

    Ahora solamente hereda de Test

--- src/main/java/tp/junit/TestSuite.java
+++ src/main/java/tp/junit/TestSuite.java
@@ -11,24 +11,27 @@ package tp.junit;
 
 import java.util.ArrayList;
 
-public class TestSuite implements Test {
+public class TestSuite extends Test {
 
     private ArrayList<Test> tests = new ArrayList<Test>();
-    private SetUp setUp;
-    
+    private SetUp setUp = null;
+
     @Override
     public void run(TestResult result) {
-        for (Test test : tests) {
-            test.run(result);
+        
+      for (Test test : tests) {
+         //Aca falta comparar si es un suite o no
+          test.setAtributos(setUp);
+         test.run(result);
+         setUp.tearDown();
         }
     }
 
     public void addTest(Test test) {
         tests.add(test);
     }
-    
-    public void addSetUp(SetUp setup){
-       setUp = setup;
+
+    public void addSetUp(SetUp setup) {
+        setUp = setup;
     }
-    
 }
