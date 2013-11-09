4360a2e4799fc0483c6d71bbfc2d37cb6e8e4e5a
francisco <fran_disa@hotmail.com>
2013-11-09 15:45:49 -0300

    getters y setters de context

--- src/main/java/testing/Context.java
+++ src/main/java/testing/Context.java
@@ -4,4 +4,12 @@ import java.util.HashMap;
 
 public class Context {
 	HashMap<String, Object> map;
+	
+	public Object getObject(String key) {
+		return map.get(key);
+	}
+	
+	public void setObject(String key, Object value) {
+		map.put(key, value);
+	}
 }
