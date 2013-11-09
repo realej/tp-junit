3851041f45182830196d1438f298b15f82bae0ee
francisco <fran_disa@hotmail.com>
2013-11-09 17:28:54 -0300

    distintos runs en el TestRunner para cada caso

--- src/main/java/testing/Test.java
+++ src/main/java/testing/Test.java
@@ -1,13 +1,24 @@
 package testing;
 
+import java.util.ArrayList;
+
 public abstract class Test {
 	protected String name;
 	protected TestSuite testSuite;
+	protected ArrayList<String> tagList;
 	
 	
 	public abstract void test();
+	
 	public String getSuiteName() {
 		return testSuite.getSuiteName();
 	}
 	
+	public ArrayList<String> getTagList() {
+		return tagList;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
--- src/main/java/testing/TestRunner.java
+++ src/main/java/testing/TestRunner.java
@@ -1,10 +1,113 @@
 package testing;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.regex.Pattern;
 
 public class TestRunner {
 	ArrayList<RunnableTest> testList;
+
+	
+	public void addTest(Test test, boolean isSkipped) {
+		RunnableTest runnableTest = new RunnableTest(test, isSkipped);
+		testList.add(runnableTest);
+	}
+	
+	private boolean matchesSuiteName(String suiteName, RunnableTest runnableTest) {
+		return suiteName == runnableTest.getSuiteName();
+	}
+	
+	public void run() {
+		for (RunnableTest runnableTest : testList) {
+			if(!runnableTest.isSkipped()) {
+				runnableTest.getTest().test();
+			}			 	
+		}
+	}
+	
+	public void runSuite(String suiteName) {
+		for (RunnableTest runnableTest : testList) {
+			if(!runnableTest.isSkipped() && matchesSuiteName(suiteName, runnableTest)) {
+				runnableTest.getTest().test();
+			}			 	
+		}
+	}
+	
+	public void runWithTagList(ArrayList<String> tagList) {
+		
+		Collections.sort(tagList);
+		
+		for (RunnableTest runnableTest : testList) {
+			ArrayList <String>testTagList = runnableTest.getTagList();
+			Collections.sort(testTagList);			
+			if(!runnableTest.isSkipped() && tagList.equals(testTagList)) {
+				runnableTest.getTest().test();
+			}			 	
+		}
+	}
+	
+	public void runWithRegularExpression(String regularExpression) {
+		Pattern pattern = Pattern.compile(regularExpression);
+		for (RunnableTest runnableTest : testList) {
+			if(!runnableTest.isSkipped() && pattern.matcher(runnableTest.getTestName()).matches()) {
+				runnableTest.getTest().test();
+			} 	
+		}
+	}
+	
+	public void runMatchingAny(String testNameRegularExpression, String testSuiteRegularExpression, ArrayList<String> tagList) {
+		Pattern testNamePattern = Pattern.compile(testNameRegularExpression);
+		Pattern testSuitePattern = Pattern.compile(testSuiteRegularExpression);		
+		Collections.sort(tagList);
+		
+		for (RunnableTest runnableTest : testList) {
+			boolean testNameMatches = testNamePattern.matcher(runnableTest.getTestName()).matches();
+			boolean suiteNameMatches = testSuitePattern.matcher(runnableTest.getSuiteName()).matches();
+			ArrayList <String>testTagList = runnableTest.getTagList();
+			Collections.sort(testTagList);
+			
+			if(!runnableTest.isSkipped() && (testNameMatches || suiteNameMatches || testTagList.equals(tagList))) {
+				runnableTest.getTest().test();
+			}			
+		}
+	}
+	
+
+	public void runMatchingAll(String testNameRegularExpression, String testSuiteRegularExpression, ArrayList<String> tagList) {
+		Pattern testNamePattern = Pattern.compile(testNameRegularExpression);
+		Pattern testSuitePattern = Pattern.compile(testSuiteRegularExpression);		
+		Collections.sort(tagList);
+		
+		for (RunnableTest runnableTest : testList) {
+			boolean testNameMatches = testNamePattern.matcher(runnableTest.getTestName()).matches();
+			boolean suiteNameMatches = testSuitePattern.matcher(runnableTest.getSuiteName()).matches();
+			ArrayList <String>testTagList = runnableTest.getTagList();
+			Collections.sort(testTagList);
+			
+			if(!runnableTest.isSkipped() && testNameMatches && suiteNameMatches && testTagList.equals(tagList)) {
+				runnableTest.getTest().test();
+			}			
+		}
+	}
+	
+	
+	
+	public void skip(String testName) {
+		for (RunnableTest runnableTest : testList) {
+			if(runnableTest.getTestName() == testName) {
+				runnableTest.setIsSkipped(true);
+			}
+		}
+	}
+	
+	public void unskip(String testName) {
+		for (RunnableTest runnableTest : testList) {
+			if(runnableTest.getTestName() == testName) {
+				runnableTest.setIsSkipped(false);
+			}
+		}
+	}	
+	
 	
 	private class RunnableTest {
 		Test test;
@@ -26,44 +129,18 @@ public class TestRunner {
 		public String getSuiteName() {
 			return test.getSuiteName();
 		}
-	}
-	
-	public void addTest(Test test, boolean isSkipped) {
-		RunnableTest runnableTest = new RunnableTest(test, isSkipped);
-		testList.add(runnableTest);
-	}
-	
-	private boolean matchesSuiteName(String suiteName, RunnableTest runnableTest) {
-		return suiteName == runnableTest.getSuiteName();
-	}
-	
-	public void run() {
-		for (RunnableTest runnableTest : testList) {
-			if(!runnableTest.isSkipped()) {
-				runnableTest.getTest().test();
-			}			 	
+		
+		public ArrayList<String> getTagList() {
+			return test.getTagList();
 		}
-	}
-	
-	public void run(String suiteName) {
-		for (RunnableTest runnableTest : testList) {
-			if(!runnableTest.isSkipped() && matchesSuiteName(suiteName, runnableTest)) {
-				runnableTest.getTest().test();
-			}			 	
+		
+		public String getTestName() {
+			return test.getName();
 		}
-	}
-	
-	public void run(ArrayList<String> tagList) {
 		
-	}
-	
-	
-	public void skip() {
-		
-	}
-	
-	public void unskip() {
-		
+		public void setIsSkipped(boolean bool) {
+			isSkipped = bool;
+		}
 	}
 	
 }
