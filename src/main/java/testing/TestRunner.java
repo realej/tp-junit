0ff61e5bfccc24cd0743972044b86606c1fbc4ba
francisco <fran_disa@hotmail.com>
2013-11-12 18:10:15 -0300

    Indentacion

--- src/main/java/testing/TestRunner.java
+++ src/main/java/testing/TestRunner.java
@@ -57,19 +57,19 @@ public class TestRunner {
 		ArrayList<SuperTest> testList = testSuite.getTestList();
 		for (SuperTest runnableTest : testList) {
 			ArrayList <String>testTagList = runnableTest.getTagList();
-				try {
-					runnableTest.testSuiteWithTagList(this, tagList);
-					if(!runnableTest.isSkiped() && tagList.equals(testTagList)) {
-						runnableTest.test();
-					}
-					}catch(AssertionException exception) {
-						//Reportar failure
-						continue;
-					}catch(Throwable error) {
-						//Reportar error
-						continue;
-						//Reportar ok
-					}
+			try {
+				runnableTest.testSuiteWithTagList(this, tagList);
+				if(!runnableTest.isSkiped() && tagList.equals(testTagList)) {
+					runnableTest.test();
+				}
+				}catch(AssertionException exception) {
+					//Reportar failure
+					continue;
+				}catch(Throwable error) {
+					//Reportar error
+					continue;
+					//Reportar ok
+				}
 		}		
 	}
 	
@@ -83,19 +83,19 @@ public class TestRunner {
 		ArrayList<SuperTest> testList = testSuite.getTestList();
 		Pattern pattern = Pattern.compile(regularExpression);
 		for (SuperTest runnableTest : testList) {
-				try {
-					runnableTest.testSuiteWithRegularExpression(this, regularExpression);
-					if(!runnableTest.isSkiped() && pattern.matcher(runnableTest.getName()).matches()) {
-						runnableTest.test();
-					}
-					}catch(AssertionException exception) {
-						//Reportar failure
-						continue;
-					}catch(Throwable error) {
-						//Reportar error
-						continue;
-						//Reportar ok
-					}
+			try {
+				runnableTest.testSuiteWithRegularExpression(this, regularExpression);
+				if(!runnableTest.isSkiped() && pattern.matcher(runnableTest.getName()).matches()) {
+					runnableTest.test();
+				}
+				}catch(AssertionException exception) {
+					//Reportar failure
+					continue;
+				}catch(Throwable error) {
+					//Reportar error
+					continue;
+					//Reportar ok
+				}
 		}
 	}
 	
@@ -111,22 +111,22 @@ public class TestRunner {
 		Pattern testSuitePattern = Pattern.compile(testSuiteRegularExpression);		
 		Collections.sort(tagList);
 		for (SuperTest runnableTest : testList) {
-				try {
-					runnableTest.testSuiteMatchingAny(this, testNameRegularExpression, testSuiteRegularExpression, tagList);
-					boolean testNameMatches = testNamePattern.matcher(runnableTest.getName()).matches();
-					boolean suiteNameMatches = testSuitePattern.matcher(runnableTest.getName()).matches();
-					ArrayList <String>testTagList = runnableTest.getTagList();
-					if(!runnableTest.isSkiped() && (testNameMatches || suiteNameMatches || testTagList.equals(tagList))) {
-						runnableTest.test();
-					}
-					}catch(AssertionException exception) {
-						//Reportar failure
-						continue;
-					}catch(Throwable error) {
-						//Reportar error
-						continue;
-						//Reportar ok
-					}
+			try {
+				runnableTest.testSuiteMatchingAny(this, testNameRegularExpression, testSuiteRegularExpression, tagList);
+		    	boolean testNameMatches = testNamePattern.matcher(runnableTest.getName()).matches();
+				boolean suiteNameMatches = testSuitePattern.matcher(runnableTest.getName()).matches();
+				ArrayList <String>testTagList = runnableTest.getTagList();
+				if(!runnableTest.isSkiped() && (testNameMatches || suiteNameMatches || testTagList.equals(tagList))) {
+					runnableTest.test();
+				}
+				}catch(AssertionException exception) {
+					//Reportar failure
+					continue;
+				}catch(Throwable error) {
+					//Reportar error
+					continue;
+					//Reportar ok
+				}
 		}
 	}
 	
@@ -142,22 +142,22 @@ public class TestRunner {
 		Pattern testSuitePattern = Pattern.compile(testSuiteRegularExpression);		
 		Collections.sort(tagList);
 		for (SuperTest runnableTest : testList) {
-				try {
-					runnableTest.testSuiteMatchingAny(this, testNameRegularExpression, testSuiteRegularExpression, tagList);
-					boolean testNameMatches = testNamePattern.matcher(runnableTest.getName()).matches();
-					boolean suiteNameMatches = testSuitePattern.matcher(runnableTest.getName()).matches();
-					ArrayList <String>testTagList = runnableTest.getTagList();
-					if(!runnableTest.isSkiped() && testNameMatches && suiteNameMatches && testTagList.equals(tagList)) {
-						runnableTest.test();
-					}
-					}catch(AssertionException exception) {
+			try {
+				runnableTest.testSuiteMatchingAny(this, testNameRegularExpression, testSuiteRegularExpression, tagList);
+				boolean testNameMatches = testNamePattern.matcher(runnableTest.getName()).matches();
+				boolean suiteNameMatches = testSuitePattern.matcher(runnableTest.getName()).matches();
+				ArrayList <String>testTagList = runnableTest.getTagList();
+				if(!runnableTest.isSkiped() && testNameMatches && suiteNameMatches && testTagList.equals(tagList)) {
+					runnableTest.test();
+				}
+				}catch(AssertionException exception) {
 						//Reportar failure
-						continue;
-					}catch(Throwable error) {
-						//Reportar error
-						continue;
-						//Reportar ok
-					}
+					continue;
+				}catch(Throwable error) {
+					//Reportar error
+					continue;
+					//Reportar ok
+				}
 		}
 	}
 	
