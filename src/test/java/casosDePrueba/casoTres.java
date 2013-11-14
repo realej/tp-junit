9c984d6336028f1e6a5ff012ac5b46b7e0b7cc1c
Sebastian <>
2013-11-14 16:31:48 -0300

    Caso 3

--- src/test/java/casosDePrueba/casoTres.java
+++ src/test/java/casosDePrueba/casoTres.java
@@ -4,10 +4,21 @@
  */
 package casosDePrueba;
 
-/**
- *
- * @author Julian
- */
+import aplicacion.TestSetUp;
+import aplicacion.testMultiplicacionCalculadora;
+import java.io.IOException;
+import tp.junit.TestRunner;
+import tp.junit.TestSuite;
+
 public class casoTres {
-    
+
+    public static void main(String[] args) throws IOException {
+        TestRunner handler = new TestRunner();
+        TestSuite suite = new TestSuite("suite");
+        testMultiplicacionCalculadora testConSetUp = new testMultiplicacionCalculadora();
+        TestSetUp setup = new TestSetUp();
+        suite.addSetUp(setup);
+        suite.addTest(testConSetUp);
+        handler.run(suite);
+    }
 }
