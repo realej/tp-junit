cb7525d45b3d1b7cb9bf13da69208c7e717ece70
Carlos.Delvalle <Carlos@Carlos-PC>
2013-11-16 16:41:04 -0300

    Commit inicial ZUnit1.1

--- src/main/java/francisco/App.java
+++ src/main/java/francisco/App.java
@@ -1,12 +1,10 @@
 package francisco;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main(String[] args) {
         System.out.println("ZUnit 1.0");
-        
-       
-    }    
-	
-}	
+
+
+    }
+}
