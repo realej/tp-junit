6ee4a059994b9fc121090cc9026b58e7a9427d1b
Sebastian <>
2013-11-21 04:21:45 -0300

    Les borre todos los import que no usan

--- src/test/java/francisco/testZunit.java
+++ src/test/java/francisco/testZunit.java
@@ -10,80 +10,78 @@ import testClasses.TestError;
 import testClasses.TestOk;
 import testClasses.TestOtraSumaOperation;
 import testClasses.TestSumaOperation;
-import testing.Assert;
 import testing.AssertionException;
 import testing.Context;
-import testing.SuiteException;
 import testing.TestSuite;
 
 public class testZunit {
 
-	@Test
-	public void nameTestOfSuiteUniqueness() {
-		TestSumaOperation firstTest = new TestSumaOperation("test suma");
-		TestOtraSumaOperation secondTest = new TestOtraSumaOperation("test suma");
-		TestSuite suite = new TestSuite("suite");
-		suite.addTest(firstTest);
-		int resultado = suite.addTest(secondTest);
-		assertTrue(resultado == 1);	
-	}
-	
-	@Test
-	public void nameSuiteOfSuiteUniqueness() {
-		TestSuite firstSuite = new TestSuite("first Suite");
-		TestSuite secondSuite = new TestSuite("first Suite");
-		TestSuite principalSuite = new TestSuite("principal suite");
-		principalSuite.addTest(firstSuite);
-		int resultado = principalSuite.addTest(secondSuite);
-		assertTrue(resultado == 1);
-	}
-	
-	@Test
-	public void setupTestSuiteSimple() {
-		SuiteOperation suite = new SuiteOperation("Suite");
-		suite.setUp();
-		Context context = suite.getContext();
-		int object = (Integer) context.getObject("uno");
-		assertTrue(object == 1);
-	}
-	
-	@Test
-	public void setupTestSuiteComplex() {
-		SuiteOperation principalSuite = new SuiteOperation("Principal suite");
-		SuiteSuma secondSuite = new SuiteSuma("Second Suite");
-		principalSuite.setUp();
-		secondSuite.setUp();
-		Context principalContext = principalSuite.getContext();
-		Context secondContext = secondSuite.getContext();
-		int object = (Integer) principalContext.getObject("uno");
-		int object2 = (Integer) secondContext.getObject("seis");
-		assertTrue(object == 1);
-		assertTrue(object2 == 6);	
-	}
-	
-	@Test
-    public void testWithError() {
-		TestError test = new TestError("test error");
-		try {
-			test.test();
-		} catch (AssertionException exception) {
-			assertTrue(false);
-		} catch (Throwable e) {
-			assertTrue(true);
-		}
-	}
+    @Test
+    public void nameTestOfSuiteUniqueness() {
+        TestSumaOperation firstTest = new TestSumaOperation("test suma");
+        TestOtraSumaOperation secondTest = new TestOtraSumaOperation("test suma");
+        TestSuite suite = new TestSuite("suite");
+        suite.addTest(firstTest);
+        int resultado = suite.addTest(secondTest);
+        assertTrue(resultado == 1);
+    }
 
-	@Test
+    @Test
+    public void nameSuiteOfSuiteUniqueness() {
+        TestSuite firstSuite = new TestSuite("first Suite");
+        TestSuite secondSuite = new TestSuite("first Suite");
+        TestSuite principalSuite = new TestSuite("principal suite");
+        principalSuite.addTest(firstSuite);
+        int resultado = principalSuite.addTest(secondSuite);
+        assertTrue(resultado == 1);
+    }
+
+    @Test
+    public void setupTestSuiteSimple() {
+        SuiteOperation suite = new SuiteOperation("Suite");
+        suite.setUp();
+        Context context = suite.getContext();
+        int object = (Integer) context.getObject("uno");
+        assertTrue(object == 1);
+    }
+
+    @Test
+    public void setupTestSuiteComplex() {
+        SuiteOperation principalSuite = new SuiteOperation("Principal suite");
+        SuiteSuma secondSuite = new SuiteSuma("Second Suite");
+        principalSuite.setUp();
+        secondSuite.setUp();
+        Context principalContext = principalSuite.getContext();
+        Context secondContext = secondSuite.getContext();
+        int object = (Integer) principalContext.getObject("uno");
+        int object2 = (Integer) secondContext.getObject("seis");
+        assertTrue(object == 1);
+        assertTrue(object2 == 6);
+    }
+
+    @Test
+    public void testWithError() {
+        TestError test = new TestError("test error");
+        try {
+            test.test();
+        } catch (AssertionException exception) {
+            assertTrue(false);
+        } catch (Throwable e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
     public void testOk() {
-		TestOk test = new TestOk("test OK");
-		try {
-			test.test();
-		} catch (AssertionException exception) {
-			assertTrue(false);
-		} catch (Throwable e) {
-			assertTrue(false);
-		}
-		assertTrue(true);
-	}
+        TestOk test = new TestOk("test OK");
+        try {
+            test.test();
+        } catch (AssertionException exception) {
+            assertTrue(false);
+        } catch (Throwable e) {
+            assertTrue(false);
+        }
+        assertTrue(true);
+    }
 
 }
